e240c02dc97ae4c97309a1ce7dc4fbc873dfcd0e
Ana Gabriela Diaz <anagaby0204@gmail.com>
2007-09-21 21:42:59 +0000

    Desarrollando la prueba para la función testExtractOrganizations.

--- plugins/scorm/tests/cases/models/scorm.test.php
+++ plugins/scorm/tests/cases/models/scorm.test.php
@@ -36,6 +36,94 @@ class CourseTestCase extends CakeTestCase {
 		$this->TestObject->parseManifest(TMP.'tests');
 	}
 	
+	
+	function testExtractOrganizations() {
+$xml = <<<eof
+	<organizations default="DMCE">
+	    <organization identifier="DMCE">
+			<title>SCORM 2004 3rd Edition Data Model Content Example 1.1</title>
+			<metadata>
+   				<adlcp:location>lesson1/lesson1MD.xml</adlcp:location>
+			</metadata>
+			<adlcp:timeLimitAction>exit,no message</adlcp:timeLimitAction>
+			<adlcp:dataFromLMS>Some SCO Information</adlcp:dataFromLMS>
+			<adlcp:completionThreshold>0.75</adlcp:completionThreshold>
+	      <item identifier="WELCOME" identifierref="res_welcome" isvisible="false" parameters="?width=500&#038;length=300>
+	        <title>Welcome</title>
+	        <adlnav:presentation>
+	          <adlnav:navigationInterface>
+	            <adlnav:hideLMSUI>continue</adlnav:hideLMSUI>
+	            <adlnav:hideLMSUI>previous</adlnav:hideLMSUI>
+	          </adlnav:navigationInterface>
+	        </adlnav:presentation>
+	        <imsss:sequencing>
+	          <imsss:controlMode 
+				choice="true" 
+				choiceExit="true" 
+				flow="true" 
+				forwardOnly="false" 
+				useCurrentAttemptObjectiveInfo = "false" 
+				useCurrentAttemptProgressInfo = "true"
+				/>
+				<imsss:sequencingRules>
+					<imsss:preConditionRule>
+						<imsss:ruleConditions conditionCombination="any">
+						   <imsss:ruleCondition 
+							referencedObjective = "some_objective_ID"
+							measureThreshold = "0.5000"
+							operator = "noOp"
+							condition = "satisfied"
+							/>
+							<imsss:ruleCondition 
+							referencedObjective = "some_objective_ID1"
+							measureThreshold = "0.8000"
+							operator = "not"
+							condition = "completed"
+							/>
+							</imsss:ruleConditions>
+						<imsss:ruleAction action = "disabled"/>
+					</imsss:preConditionRule>
+	
+					<imsss:postConditionRule>
+					   <imsss:ruleConditions>
+					      <imsss:ruleCondition condition="satisfied"/>
+					   </imsss:ruleConditions>
+					   <imsss:ruleAction action="exitParent"/>
+					</imsss:postConditionRule>
+					<imsss:exitConditionRule>
+					   <imsss:ruleConditions>
+					      <imsss:ruleCondition condition="satisfied"/>
+					   </imsss:ruleConditions>
+					   <imsss:ruleAction action="exit"/>
+					</imsss:exitConditionRule>
+				</imsss:sequencingRules>
+				<imsss:limitConditions attemptLimit="1" attemptAbsoluteDurationLimit="4 days"/><!-- xs>duration ??? --!>
+				<imsss:rollupRules rollupObjectiveSatisfied = "true" rollupProgressCompletion = "true" objectiveMeasureWeight = "1.0000">
+				   <imsss:rollupRule childActivitySet = "all" minimumCount = "0" minimumPercent = "0.0000" >
+						<imsss:rollupConditions conditionCombination = "any">
+						   <imsss:rollupCondition condition = "attempted" operator = "noOp"/>
+						</imsss:rollupConditions>
+				      <imsss:rollupAction action = "completed"/>
+				   </imsss:rollupRule>
+				</imsss:rollupRules>
+			<imsss:objectives>
+			<imsss:primaryObjective objectiveID = "PRIMARYOBJ" satisfiedByMeasure = "true">
+				<imsss:minNormalizedMeasure>0.6</imsss:minNormalizedMeasure>
+					<imsss:mapInfo
+								targetObjectiveID = "obj_module_1"
+								readSatisfiedStatus="false"
+					          	readNormalizedMeasure = "false"
+					          	writeSatisfiedStatus = "true"
+								writeNormalizedMeasure="false"/>
+	        </imsss:sequencing>
+	      </item>
+		</organization>
+	</organizations>
+eof;
+	debug(new XML($xml));
+		
+	}
+
 	function testGetSchemaVersion() {
 		$manifest = new XMLNode('manifest');
 		$metadata = new XMLNode('metadata');
