1c640f1159697884768941748f92783f0b03d80f
José Lorenzo Rodríguez <jose.zap@gmail.com>
2007-09-21 19:34:02 +0000

    Más pruebas unitarias en ScormPlugin.Scrom model

--- plugins/scorm/tests/cases/models/scorm.test.php
+++ plugins/scorm/tests/cases/models/scorm.test.php
@@ -36,5 +36,77 @@ class CourseTestCase extends CakeTestCase {
 		$this->TestObject->parseManifest(TMP.'tests');
 	}
 	
+	function testGetSchemaVersion() {
+		$manifest = new XMLNode('manifest');
+		$metadata = new XMLNode('metadata');
+		$schema = new XMLNode('schema', null, 'ADL SCORM');
+		$schemaversion = new XMLNode('schemaversion', null, '2004 3rd Edition');
+		$metadata->append($schemaversion);
+		$metadata->append($schema);
+		$manifest->append($metadata);
+		$this->assertEqual(
+			$this->TestObject->getSchemaVersion($manifest),
+			'2004 3rd Edition'
+		);
+	}
+
+	function testExtractResources() {
+		$resources = new XMLNode('resources');
+		$resource1 = new XMLNode('resource', array(
+				'identifier' => 'RESOURCE1',
+				'adlcp:scormType' => 'sco',
+				'type' => 'webcontent',
+				'href' => 'localitem1.html'
+			)
+		);
+		$resources->append($resource1);
+		$resource2 = new XMLNode('resource', array(
+				'identifier' => 'RESOURCE2',
+				'adlcp:scormType' => 'sco',
+				'type' => 'webcontent',
+				'href' => 'localitem2.html'
+			)
+		);
+		$resources->append($resource2);
+		$resource3 = new XMLNode('resource', array(
+				'identifier' => 'RESOURCE3',
+				'adlcp:scormType' => 'sco',
+				'type' => 'webcontent',
+				'href' => 'localitem3.html'
+			)
+		);
+		$file = new XMLNode('file', array('href' => 'localitem3.html')); //Ignored
+		$resource3->append($file);
+		$resources->append($resource3);
+		
+		$manifest = new XMLNode('manifest');
+		$manifest->append($resources);
+		
+		
+		$this->assertEqual(
+			$this->TestObject->extractResources($manifest),
+			array (
+			    'RESOURCE1' => array (
+			            'identifier' => 'RESOURCE1',
+			            'adlcp:scormType' => 'sco',
+			            'type' => 'webcontent',
+			            'href' => 'localitem1.html',
+		        ),
+			    'RESOURCE2' => array (
+			            'identifier' => 'RESOURCE2',
+			            'adlcp:scormType' => 'sco',
+			            'type' => 'webcontent',
+			            'href' => 'localitem2.html'
+		        ),
+			    'RESOURCE3' => array (
+			            'identifier' => 'RESOURCE3',
+			            'adlcp:scormType' => 'sco',
+			            'type' => 'webcontent',
+			            'href' => 'localitem3.html'
+		        )
+
+			)
+		);
+	}
 }
 ?>
