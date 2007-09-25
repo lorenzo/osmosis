c9d1baed87f3c9b244c6f830acf64b7a4b03f6e8
Joaquín Windmüller <joaquin.win@gmail.com>
2007-09-25 01:44:24 +0000

    svn:ignore a los directorios dentro de tmp

--- models/course.php
+++ models/course.php
@@ -3,28 +3,27 @@ class Course extends AppModel {
 
 	var $name = 'Course';
 	var $validate = array(
-		'department_id'		=> array(
-										'Error.numeric' => array('rule'=>'numeric'),
-										'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
-									),
-		'owner_id' 			=> array(
-										'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
-										'Error.numeric' => array('rule'=>'numeric')
-									),
+		'department_id' => array(
+			'Error.numeric' => array('rule'=>'numeric'),
+			'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
+		),
+		'owner_id' => array(
+			'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
+			'Error.numeric' => array('rule'=>'numeric')
+		),
 		'code' 				=> array(
-										'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
-										'Error.maxlength' => array('rule' => array('maxlength', 10)),
-										'Error.alphanumeric' => array('rule'=>'alphaNumeric')
-									),
+			'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
+			'Error.maxlength' => array('rule' => array('maxlength', 10)),
+			'Error.alphanumeric' => array('rule'=>'alphaNumeric')
+		),
 		'name' 				=> array(
-										'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
-										'Error.maxlength' => array('rule' => array('maxlength',150))
-										//TODO: No HTML
-									),
+			'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
+			'Error.maxlength' => array('rule' => array('maxlength',150))
+			//TODO: No HTML
+		),
 		'description' 		=> array(
-										'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
-										
-									)
+			'Error.empty' => array('rule'=>'/.+/','required'=>true,'on'=>'create','message'=>'Error.empty'),
+		)
 	);
 	
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
