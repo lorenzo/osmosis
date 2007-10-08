be76f04c93436c25a64ee2cb13cdba8c7bc15227
Joaquín Windmüller <joaquin.win@gmail.com>
2007-10-08 00:41:37 +0000

    Esquema de la base de datos con los modelos Rule, Rollup y Condition.

--- config/sql/osmosis.sql
+++ config/sql/osmosis.sql
@@ -1,25 +1,46 @@
 -- phpMyAdmin SQL Dump
--- version 2.10.0.2
+-- version 2.11.0
 -- http://www.phpmyadmin.net
--- 
+--
 -- Host: localhost
--- Generation Time: Sep 05, 2007 at 02:10 PM
+-- Generation Time: Oct 07, 2007 at 08:33 PM
 -- Server version: 5.0.37
--- PHP Version: 5.2.1
+-- PHP Version: 5.2.2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
--- 
--- Database: `osmosis`
--- 
+--
+-- Database: `osmosis2`
+--
 
 -- --------------------------------------------------------
 
--- 
--- Table structure for table `courses`
--- 
+--
+-- Table structure for table `conditions`
+--
 
-CREATE TABLE `courses` (
+CREATE TABLE IF NOT EXISTS `conditions` (
+  `id` int(11) NOT NULL auto_increment,
+  `referencedObjective` varchar(255) default NULL,
+  `measureThreshold` varchar(7) default NULL,
+  `operator` varchar(4) default 'noOp',
+  `codition` varchar(27) NOT NULL,
+  `rule_id` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `conditions`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `department_id` int(4) unsigned NOT NULL,
   `owner_id` int(11) NOT NULL,
@@ -30,44 +51,119 @@ CREATE TABLE `courses` (
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
--- 
+--
 -- Dumping data for table `courses`
--- 
+--
 
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `departments`
--- 
+--
 
-CREATE TABLE `departments` (
+CREATE TABLE IF NOT EXISTS `departments` (
   `id` int(4) unsigned NOT NULL auto_increment,
   `name` varchar(150) collate utf8_unicode_ci NOT NULL,
   `description` text collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
--- 
+--
 -- Dumping data for table `departments`
--- 
+--
 
-INSERT INTO `departments` VALUES (1, 'Departamento de BiologÃ­a Celular', 'Todas las asignaturas cuyos cÃ³digos sean de la forma BC, EP, GP.');
+INSERT INTO `departments` (`id`, `name`, `description`) VALUES
+(1, 'Departamento de BiologÃ­a Celular', 'Todas las asignaturas cuyos cÃ³digos sean de la forma BC, EP, GP.');
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `plugins`
--- 
+--
 
-CREATE TABLE `plugins` (
+CREATE TABLE IF NOT EXISTS `plugins` (
   `id` smallint(4) unsigned NOT NULL auto_increment,
   `name` varchar(50) collate utf8_unicode_ci NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Holds installed plugin names and active status' AUTO_INCREMENT=1 ;
 
--- 
+--
 -- Dumping data for table `plugins`
--- 
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rollups`
+--
+
+CREATE TABLE IF NOT EXISTS `rollups` (
+  `id` int(11) NOT NULL auto_increment,
+  `rollupObjectiveSatisfied` varchar(5) default 'true',
+  `rollupProgressCompletion` varchar(5) default 'true',
+  `objectiveMeasureWeight` varchar(20) default '1.0000',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `rollups`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rules`
+--
+
+CREATE TABLE IF NOT EXISTS `rules` (
+  `id` int(11) NOT NULL auto_increment,
+  `type` varchar(4) default NULL,
+  `conditionCombination` varchar(3) default 'all',
+  `action` varchar(20) default NULL,
+  `minimumPercent` varchar(6) default '0.0000',
+  `minimumCount` varchar(5) default '0',
+  `rollup_id` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `rules`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schema_info`
+--
+
+CREATE TABLE IF NOT EXISTS `schema_info` (
+  `version` int(10) unsigned NOT NULL default '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `schema_info`
+--
+
+INSERT INTO `schema_info` (`version`) VALUES
+(1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scorms`
+--
+
+CREATE TABLE IF NOT EXISTS `scorms` (
+  `id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `scorms`
+--
 
