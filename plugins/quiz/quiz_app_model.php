eb28dbdfd3b27440dad8859dc98dc0f8df4488bf
Joaquín Windmüller <joaquin.win@gmail.com>
2008-02-02 16:51:46 +0000

    Se elimina la funcion save, porque ahora cakePHP maneja automaticamente el guardar asociaciones with

--- plugins/quiz/quiz_app_model.php
+++ plugins/quiz/quiz_app_model.php
@@ -1,23 +1,5 @@
 <?php
 class QuizAppModel extends AppModel {
 	
-	function save($data,$validate=true,$fieldList=array()) {
-		if(!$result = parent::save($data,$validate,$fieldList)) {
-			return false;
-		}else {
-			$assocs = $this->getAssociated();
-			foreach ($assocs as $key => $type) {
-				if(isset($data[$key]) && $type == 'hasAndBelongsToMany' && isset($this->hasAndBelongsToMany[$key]['with'])) {
-					$with = $this->hasAndBelongsToMany[$key]['with'];
-					$assocData[$key] = $data[$key];
-					unset($assocData[$key]['id']);
-					$assocData[$key][$this->hasAndBelongsToMany[$key]['foreignKey']] = $this->id;
-					$assocData[$key][$this->hasAndBelongsToMany[$key]['associationForeignKey']] = $data[$key]['id'];
-					$this->{$with}->save($assocData,$validate,$fieldList);
-				}
-			}
-		}
-		return $result;
-	}
 }
 ?>
