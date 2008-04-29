d38746e9c8bf6148b09129dd75e8e786ec95b7d9
José Lorenzo Rodríguez <jose.zap@gmail.com>
2008-04-29 01:56:13 +0000

    Eliminando el plugin lockers para hacer bake nuevamente

--- plugins/locker/config/description.php
+++
@@ -1,5 +0,0 @@
-<?php
-Configure::write('Locker.description','Lorem ipsum dolor sit amet, consectetuer adipiscing elit.');
-Configure::write('Locker.title','Locker');
-Configure::write('Locker.type',array('other'));
-?>
