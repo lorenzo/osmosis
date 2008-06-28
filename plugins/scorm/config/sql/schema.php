e1615d160b757b623039f67343c356a89440f8c4
José Lorenzo Rodríguez <jose.zap@gmail.com>
2008-06-28 05:21:25 +0000

    Agregando tabla faltante al schema de scorm

--- plugins/scorm/config/sql/schema.php
+++ plugins/scorm/config/sql/schema.php
@@ -121,5 +121,23 @@ class ScormSchema extends CakeSchema {
 			'path' => array('type'=>'text', 'null' => false),
 			'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
 		);
+	var $scorm_scos = array(
+			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+			'scorm_id' => array('type'=>'integer', 'null' => false),
+			'parent_id' => array('type'=>'integer', 'null' => true, 'default' => NULL),
+			'manifest' => array('type'=>'string', 'null' => false),
+			'organization' => array('type'=>'string', 'null' => false),
+			'identifier' => array('type'=>'string', 'null' => false),
+			'href' => array('type'=>'string', 'null' => true, 'default' => NULL),
+			'title' => array('type'=>'string', 'null' => false),
+			'completionThreshold' => array('type'=>'string', 'null' => true, 'default' => NULL, 'length' => 3),
+			'parameters' => array('type'=>'text', 'null' => true, 'default' => NULL),
+			'isvisible' => array('type'=>'string', 'null' => false, 'default' => 'true', 'length' => 5),
+			'attemptAbsoluteDurationLimit' => array('type'=>'string', 'null' => true, 'default' => NULL, 'length' => 6),
+			'dataFromLMS' => array('type'=>'text', 'null' => true, 'default' => NULL),
+			'attemptLimit' => array('type'=>'string', 'null' => true, 'default' => NULL, 'length' => 10),
+			'scormType' => array('type'=>'string', 'null' => true, 'default' => NULL, 'length' => 6),
+			'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+		);
 }
 ?>
